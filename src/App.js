9fb3f4b37ac860e909ee18ae6df044da8158ab1a
Delving-Mage <147742069+Delving-Mage@users.noreply.github.com>
2023-10-13 00:20:24 +0530

    added deploy sets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
       Hey, Ajay here.
+      <button onClick={console.log("hey")}>ONŃ</button>
       </header>
     </div>
   );
